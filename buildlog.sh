6a671cdc7e409fa3f8ed72f3630f6e2b64b57d54
lbdroid <definesinsanity@gmail.com>
2014-01-02 21:13:16 -0500

    Simplify

--- buildlog.sh
+++ buildlog.sh
@@ -6,19 +6,15 @@ prevts=
 for ts in `ls -l /var/lib/jenkins/jobs/apexq-staging-4.4/builds/ | grep drwx | cut -c48-`; do
 
 export ts
-(echo "==================================="
-echo "Since $ts" 
-echo "==================================="
+(echo "===================================" | tee >> $CHANGESPATH
+echo "Since $ts" | tee >> $CHANGESPATH
+echo "===================================" | tee >> $CHANGESPATH
 if [ -z "$prevts" ]; then
-  repo forall -c 'L=$(git log --oneline --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts; fi'
+  repo forall -c 'L=$(git log --oneline --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts; fi' | tee >> $CHANGESPATH
 else
-  repo forall -c 'L=$(git log --oneline --since $ts --until $prevts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts --until $prevts; fi'
+  repo forall -c 'L=$(git log --oneline --since $ts --until $prevts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts --until $prevts; fi' | tee >> $CHANGESPATH
 fi
 echo)
 export prevts=$ts
 
-done | tee > $CHANGESPATH
-
-if [ -z "$prevts" ]; then
-  echo "This is the first build of this type for device $CM_BUILD" >> $CHANGESPATH
-fi
+done
