7045bcc069bc75adf20d0873097062fbf024718c
lbdroid <definesinsanity@gmail.com>
2014-01-03 10:19:09 -0500

    unix time

--- buildlog.sh
+++ buildlog.sh
@@ -3,11 +3,11 @@ export CHANGESPATH=$WORKSPACE/CHANGES.txt
 rm $CHANGESPATH 2>/dev/null
 
 prevts=
-for ts in `ls -lt /var/lib/jenkins/jobs/apexq-staging-4.4/builds/ | grep drwx | tail -n+1 | cut -c48-`; do
+for ts in `ls -lt --time-style=+%s /var/lib/jenkins/jobs/apexq-staging-4.4/builds/ | grep drwx | tail -n+1 | cut -c36-45`; do
 
 export ts
 (echo "===================================" | tee >> $CHANGESPATH
-echo "Since $ts" | tee >> $CHANGESPATH
+echo -n "Since ";date -u -d @$ts 
 echo "===================================" | tee >> $CHANGESPATH
 if [ -z "$prevts" ]; then
   repo forall -c 'L=$(git log --oneline --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts; fi' | tee >> $CHANGESPATH
