c63b6e23803218449151e490b840862fa918bb4a
lbdroid <definesinsanity@gmail.com>
2014-01-03 10:41:59 -0500

    Wrap it up, changelog :)

--- buildlog.sh
+++ buildlog.sh
@@ -6,15 +6,15 @@ prevts=
 for ts in `ls -lt --time-style=+%s /var/lib/jenkins/jobs/apexq-staging-4.4/builds/ | grep drwx | tail -n+1 | cut -c35-45`; do
 
 export ts
-(echo "===================================" | tee >> $CHANGESPATH
+(echo "==================================="
 echo -n "Since $ts : ";date -u -d @$ts 
-echo "===================================" | tee >> $CHANGESPATH
+echo "==================================="
 if [ -z "$prevts" ]; then
-  repo forall -c 'L=$(git log --oneline --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts; fi' | tee >> $CHANGESPATH
+  repo forall -c 'L=$(git log --oneline --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts; fi'
 else
-  repo forall -c 'L=$(git log --oneline --since $ts --until $prevts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts --until $prevts; fi' | tee >> $CHANGESPATH
+  repo forall -c 'L=$(git log --oneline --since $ts --until $prevts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts --until $prevts; fi'
 fi
-echo)
+echo) >> $CHANGESPATH
 export prevts=$ts
 
 done
