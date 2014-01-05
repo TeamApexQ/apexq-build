0cb42019c4d9696615d9a30703b992db87b9bf26
lbdroid <definesinsanity@gmail.com>
2014-01-05 13:32:20 -0500

    Distinguish between changes-since and local-changes

--- buildlog.sh
+++ buildlog.sh
@@ -2,12 +2,17 @@ MYPATH=$(dirname $0)
 export CHANGESPATH=$WORKSPACE/CHANGES.txt
 rm $CHANGESPATH 2>/dev/null
 
+i=1
 prevts=
 for ts in `ls -lt --time-style=+%s /var/lib/jenkins/jobs/apexq-staging-4.4/builds/ | grep drwx | cut -c35-45`; do
 
 export ts
 (echo "==================================="
-echo -n "Since $ts : ";date -u -d @$ts 
+if [ $i = 1 ]; then
+  echo "Locally applied changes"
+else
+  echo -n "Since $ts : ";date -u -d @$ts 
+fi
 echo "==================================="
 if [ -z "$prevts" ]; then
   repo forall -c 'L=$(git log --oneline --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts; fi'
@@ -16,5 +21,6 @@ else
 fi
 echo) >> $CHANGESPATH
 export prevts=$ts
+i=$((i+1))
 
 done
