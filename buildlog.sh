f8c70548226226539b09efee4f321225fc9d5983
lbdroid <lbdroid@users.noreply.github.com>
2014-02-11 12:54:16 -0500

    Variable path to job root

--- buildlog.sh
+++ buildlog.sh
@@ -4,7 +4,7 @@ rm $CHANGESPATH 2>/dev/null
 
 i=1
 prevts=
-for ts in `ls -lt --time-style=+%s /var/lib/jenkins/jobs/apexq-staging/builds/ | grep drwx | cut -c35-45`; do
+for ts in `ls -lt --time-style=+%s $JOB_ROOT/builds/ | grep drwx | cut -c35-45`; do
 
 export ts
 (echo "==================================="
@@ -15,9 +15,9 @@ else
 fi
 echo "==================================="
 if [ -z "$prevts" ]; then
-  /var/lib/jenkins/jobs/apexq-staging/bin/repo forall -c 'L=$(git log --oneline --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts; fi'
+  $JOB_ROOT/bin/repo forall -c 'L=$(git log --oneline --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts; fi'
 else
-  /var/lib/jenkins/jobs/apexq-staging/bin/repo forall -c 'L=$(git log --oneline --since $ts --until $prevts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts --until $prevts; fi'
+  $JOB_ROOT/bin/repo forall -c 'L=$(git log --oneline --since $ts --until $prevts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts --until $prevts; fi'
 fi
 echo) >> $CHANGESPATH
 export prevts=$ts
